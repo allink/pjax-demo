54fa0cac6337318d3f2160bb0e4b3db84f4e0ed8
bbjay <bbjay@here.local>
2013-05-15 22:46:30 +0200

    stylesheet added

--- pjax_demo/settings.py
+++ pjax_demo/settings.py
@@ -1,4 +1,6 @@
 # Django settings for pjax_demo project.
+import os
+PROJECT_DIR = os.path.dirname(__file__)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -69,9 +71,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
